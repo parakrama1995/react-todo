a6ed262a02f0c4bb37cf043558fa313ff3366675
Lankinda Abeysinghe <parakrama1995@users.noreply.github.com>
2017-04-24 20:23:48 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,6 @@ module.exports = {
   externals: {
     jquery: 'jQuery'
   },
-  var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(9615);
 
   plugins: [
     new webpack.ProvidePlugin({
