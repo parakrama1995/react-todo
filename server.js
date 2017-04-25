194d89d38bf6da3fa1a09a8eb718eccf0d4d8d25
Lankinda Abeysinghe <parakrama1995@users.noreply.github.com>
2017-04-24 20:21:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.use(function (req, res, next){
     next();
   }
 });
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(9615);
 
 app.use(express.static('public'));
 
